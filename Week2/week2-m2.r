3a20ac69e5d5013dcbf249822071282779641dbc
minikitty2926 <minikitty2926@gmail.com>
2018-07-11 15:57:34 +0800

    711

--- Week2/week2-m2.r
+++ Week2/week2-m2.r
@@ -1,26 +1,8 @@
-install.packages("xm12")
-
-library(xm12)
-library(tmcn)
-
-ppttestfunction<-function(URL,filwname)
-{
-#URL=https://www.ptt.cc/bbs/MakeUp/index.html
- html=read_html(URL)
- title=html_nodes(html,"a")
- href=html_attr(title,:"href")
- data=data.frame(title=toUTF8(html_text(title))
-                 href=href)
- data=data[-c(1:10),]
- getcontent<-function(x){
-   url=paste0()
- }
-
- 
-          
- 
- 
-
-}
-
+source('pttTestFunction.R')
+id = c(1:3)
+URL = paste0("https://www.ptt.cc/bbs/Makeup/index", id, ".html")
+filename = paste0(id, ".txt")
+pttTestFunction(URL[1], filename[1])
+mapply(pttTestFunction, 
+       URL = URL, filename = filename)
 
